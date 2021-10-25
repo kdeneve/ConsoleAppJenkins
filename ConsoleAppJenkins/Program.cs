069db8d847377795f3de81a9e65d38fecf783cec
Kurt <kdeneve@gmail.com>
2021-10-25 21:52:26 +0200

    Hello Hogent ipv World

--- ConsoleAppJenkins/Program.cs
+++ ConsoleAppJenkins/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppJenkins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello HoGent!");
         }
     }
 }

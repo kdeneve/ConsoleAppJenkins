e100d055602dcb504785cd70553c4ee0ba8266b9
Kurt <kdeneve@gmail.com>
2021-10-26 20:54:14 +0200

    tijdens meeting

--- ConsoleAppJenkins/Program.cs
+++ ConsoleAppJenkins/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleAppJenkins
             Console.WriteLine("Hello Kurt!");
             Console.WriteLine("en groepsleden!");
             Console.WriteLine("net voor meeting!");
+            Console.WriteLine("tijdens meeting!");
         }
     }
 }
